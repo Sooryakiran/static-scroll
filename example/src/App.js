5031f7971bcd4d317ca8c1d634d2ae4fe55080e1
Sooryakiran <sooryakiranpallikulathil@gmail.com>
2024-06-22 12:57:48 +0530

    initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,215 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-import { ExampleComponent } from 'static-scroll'
-import 'static-scroll/dist/index.css'
+import StaticScroll from 'static-scroll'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [slide1, setSlide1] = useState(0)
+  const [slide2, setSlide2] = useState(0)
+  const [slide3, setSlide3] = useState(0)
+
+  const [slide4, setSlide4] = useState(0)
+  const [slide5, setSlide5] = useState(0)
+  const [slide6, setSlide6] = useState(0)
+
+  const videoRef = useRef(null)
+
+  useEffect(() => {
+    const video = videoRef.current
+    if (video) {
+      const duration = video.duration;
+      const time = duration * slide6 || 0
+      console.log(time, duration)
+
+      video.currentTime = time
+    }
+  }, [slide6])
+
+  return (
+    <>
+      <StaticScroll updateScroll={(x) => setSlide1(x)}>
+        <div className='h-100 w-100 red d-flex center'>
+          <div className='white-text'>
+            <h1 style={{ transform: "scale(" + (1 + slide1 * 5) + ")" }}>
+              Scroll down to start
+            </h1>
+          </div>
+          <div className='codeblock'>
+            <pre>
+              <code>
+                {`<StaticScroll updateScroll={(x) => setSlide(x)}>
+  <div className='h-100 w-100 red d-flex center'>
+    <div className='white-text'>
+      <h1 style={{transform: "scale(" + (1 + slide * 5) + ")"}}>
+        Scroll down to start
+      </h1>
+    </div>
+  </div>
+</StaticScroll>`}
+              </code>
+            </pre>
+          </div>
+        </div>
+      </StaticScroll>
+      <StaticScroll updateScroll={(x) => setSlide2(x)}>
+        <div className='h-100 w-100 d-flex center blue relative'>
+          <div style={{
+            top: "50%",
+            left: (100 * slide2) + "%"
+          }} className='abs-ball'>
+          </div>
+          <div className='white-text'>
+            <h1>AAAAAnimate anything.
+            </h1>
+          </div>
+          <div className='codeblock'>
+            <pre>
+              <code>
+                {`<StaticScroll updateScroll={(x) => setSlide(x)}>
+  <div className='h-100 w-100 d-flex center blue relative'>
+    <div style={{
+      top: "50%",
+      left: (100 * slide) + "%"
+    }} className='abs-ball'>
+    </div>
+    <div className='white-text'>
+      <h1>AAAAAnimate anything.
+      </h1>
+    </div>
+  </div>
+</StaticScroll>`}
+              </code>
+            </pre>
+          </div>
+        </div>
+      </StaticScroll>
+
+      <StaticScroll numPages={6} updateScroll={(x) => setSlide3(x)}>
+        <div className='h-100 w-100 d-flex center gray relative'>
+          <div style={{
+            top: "50%",
+            left: (100 * slide3) + "%"
+          }} className='abs-ball'>
+          </div>
+          <div className='white-text'>
+            <h1>slower?
+            </h1>
+          </div>
+          <div className='codeblock'>
+            <pre>
+              <code>
+                {`<StaticScroll numPages={6} updateScroll={(x) => setSlide(x)}>
+  <div className='h-100 w-100 d-flex center gray relative'>
+    <div style={{
+      top: "50%",
+      left: (100 * slide) + "%"
+    }} className='abs-ball'>
+    </div>
+    <div className='white-text'>
+      <h1>slower?
+      </h1>
+    </div>
+  </div>
+</StaticScroll>`}
+              </code>
+            </pre>
+          </div>
+        </div>
+      </StaticScroll>
+
+      <StaticScroll numPages={1.4} updateScroll={(x) => setSlide4(x)}>
+        <div className='h-100 w-100 d-flex center purple relative'>
+          <div style={{
+            top: "50%",
+            left: (100 * slide4) + "%"
+          }} className='abs-ball'>
+          </div>
+          <div className='white-text'>
+            <h1>faster?
+            </h1>
+          </div>
+          <div className='codeblock'>
+            <pre>
+              <code>
+                {`<StaticScroll numPages={1.4} updateScroll={(x) => setSlide(x)}>
+  <div className='h-100 w-100 d-flex center purple relative'>
+    <div style={{
+      top: "50%",
+      left: (100 * slide) + "%"
+    }} className='abs-ball'>
+    </div>
+    <div className='white-text'>
+      <h1>slower?
+      </h1>
+    </div>
+  </div>
+</StaticScroll>`}
+              </code>
+            </pre>
+          </div>
+        </div>
+      </StaticScroll>
+
+      <StaticScroll updateScroll={(x) => setSlide5(Math.round(x * 2))}>
+        <div className='h-100 w-100 d-flex center'>
+          <div className='signalparent'>
+            <div style={{ opacity: (slide5 == 0) ? "1" : "0.3" }} className='redsignal'></div>
+            <div style={{ opacity: (slide5 == 1) ? "1" : "0.3" }} className='yellowsignal'></div>
+            <div style={{ opacity: (slide5 == 2) ? "1" : "0.3" }} className='greensignal'></div>
+          </div>
+          <div>
+            <center>
+              <h1>
+                ready, set, scrolllllll
+              </h1>
+              <p>
+                discretize
+              </p>
+            </center>
+          </div>
+          <div className='codeblock'>
+            <pre>
+              <code>
+                {`
+      <StaticScroll updateScroll={(x) => setSlide(Math.round(x * 2))}>
+        <div className='h-100 w-100 d-flex center'>
+          <div className='signalparent'>
+            <div style={{ opacity: (slide == 0) ? "1" : "0.3" }} className='redsignal'></div>
+            <div style={{ opacity: (slide == 1) ? "1" : "0.3" }} className='yellowsignal'></div>
+            <div style={{ opacity: (slide == 2) ? "1" : "0.3" }} className='greensignal'></div>
+          </div>
+          <div>
+            <center>
+              <h1>
+                ready, set, scrolllllll
+              </h1>
+              <p>
+                discretize
+              </p>
+            </center>
+          </div>
+        </div>
+      </StaticScroll>`}
+              </code>
+            </pre>
+          </div>
+        </div>
+      </StaticScroll>
+
+      <StaticScroll numPages={6.25} updateScroll={(x) => setSlide6(x)}>
+        <div className='h-100 w-100 black d-flex center relative'>
+          <video ref={videoRef} width="100%" height="100%">
+            <source src="/thee.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <div className='white-text abs-cent'>
+            <h1 style={{transform: "scale(" + (1 + 5 * slide6) + ")"}}>or show video frames</h1>
+          </div>
+        </div>
+      </StaticScroll>
+
+
+    </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,99 @@
-import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+import React, { useEffect, useRef } from 'react'
+
+const StaticScroll = ({ children, numPages = 3, updateScroll = () => 0, numSnaps = null }) => {
+  ////////////////////////////////////////////////////////////////////////////////////////
+  // 
+  // this component wraps around the elements that appear in a page. 
+  // each page has a height of 100vh.
+  // numPages is the number of virtual pages the user must scroll to reach the end
+  // of the component. num snaps lets you do scroll snapping if you have snapping enabled.
+  // updateScroll will callback with a value from 0 to 1
+  //
+  //
+  // author: soorkie
+  // email: captain@soorkie.com
+  //
+  ////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+  // lets define some basic constants
+  const ZERO = 0
+  const ONE = 1
+  const VIRTUAL_HEIGHT = (numPages * 100) + "vh"
+  const FULL_HEIGHT = "100vh"
+  const FULL_WIDTH = "100vw"
+
+  // the component consists of two divs, one inside the other
+  // the outer div is a relative full width one with height equal to numPages * 100vh.
+  // the inner div is a sticky component that stays in the place. it has a height of 100vh.
+  const OUTER_DIV_STYLE = {
+    position: "relative",
+    overflow: "clip",
+    height: VIRTUAL_HEIGHT
+  }
+
+  const INNER_DIV_STYLE = {
+    position: "sticky",
+    top: ZERO,
+    width: FULL_WIDTH,
+    minHeight: FULL_HEIGHT
+  }
+
+
+
+
+  // we define the ref to parent and child to handle scroll updates
+  const parentRef = useRef(null)
+  const childRef = useRef(null)
+
+  // setup hooks for handling scroll updates
+  const updatePosition = () => {
+    if (parentRef.current) {
+      const rect = parentRef.current.getBoundingClientRect();
+      const target = Math.min(Math.max(-rect.top, ZERO), window.innerHeight * (numPages - ONE))
+      updateScroll(target / window.innerHeight / (numPages - ONE))
+    }
+  };
+
+  // attach the call back hook to scroll or resize
+  useEffect(() => {
+    window.addEventListener('scroll', updatePosition);
+    window.addEventListener('resize', updatePosition);
+    updatePosition();
+    return () => {
+      window.removeEventListener('scroll', updatePosition);
+      window.removeEventListener('resize', updatePosition);
+    };
+  }, []);
+
+  // if the user needs window snapping
+  // this component will not work if the user has already enabled window snapping. 
+  // so as a work around, we allow the user to specify snap points
+  const snapAssist = numSnaps ? Array.from(Array(numSnaps - 1).keys())
+    .map((each, key) => (
+      <div key={key} style={
+        {
+          scrollSnapAlign: "start",
+          scrollSnapStop: "always",
+          position: "absolute",
+          top: key * 100 * numPages / numSnaps + "vh",
+          height: 100 * numPages / numSnaps + "vh",
+          opacity: 0,
+          zIndex: "-1"
+        }}>
+        DEBUG RELATIVE SCROLL
+      </div>)) : ""
+
+  return (
+    <div ref={parentRef} style={OUTER_DIV_STYLE}>
+      <div ref={childRef} style={INNER_DIV_STYLE}>
+        {children}
+      </div>
+      {snapAssist}
+    </div>
+  )
 }
+
+export default StaticScroll;
